6216bc56d3739e98f7cabe90272162de8f550329
Ashish <ashish.thete@deccanlogic.com>
2024-01-14 10:26:49 +0530

    inginx testing

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(routers.router, prefix="/api")
+app.include_router(routers.router, prefix="/educatu-server/api")
 
 @app.get("/")
 async def root():
