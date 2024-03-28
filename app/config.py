0a817c8b09cd8d3891a279f66ff8ec74128cec1e
Priyanka Jain <priyanka.bondarde@deccanlogic.com>
2024-03-28 09:30:18 +0530

    minor changes with req.txt

--- app/config.py
+++ app/config.py
@@ -13,4 +13,4 @@ API_PREFIX = "/educatu-server/api"
 TOKEN_SECRET_KEY = "knm5e5xxyxe6g8ufmnwk"
 TOKEN_ALGORITHM = "HS256"
 TOKEN_EXPIRE_MINUTES = 120   # in min
-DATABASE_URL = "postgresql://postgres:postgres@localhost/educatu"
+DATABASE_URL = "postgresql://postgres:root@localhost/educatu"
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 myenv\Scripts\activate
 uvicorn app.main:app --reload
 
-
 dependencies
 
+py -m venv myenv
 pip install fastapi
 pip install "uvicorn[standard]"
 pip install "python-jose[cryptography]"
